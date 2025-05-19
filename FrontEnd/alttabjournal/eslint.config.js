40a03fc19076e22a7385586f0d567dd42ab6835c
yaswanthr7-sn <yaswanthr7@gmail.com>
2025-05-19 15:02:29 +0530

    Applying Lint and using CodeQl instead of SonarQube

---
+++ FrontEnd/alttabjournal/eslint.config.js
@@ -0,0 +1,33 @@
+import js from "@eslint/js";
+import react from "eslint-plugin-react";
+import jsxA11y from "eslint-plugin-jsx-a11y";
+
+export default [
+  js.configs.recommended,
+  {
+    plugins: {
+      react,
+      jsxA11y
+    },
+    files: ["src/**/*.js", "src/**/*.jsx"],
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        ecmaFeatures: {
+          jsx: true
+        }
+      }
+    },
+    rules: {
+      // If you're using the automatic JSX runtime, you can disable this:
+      "react/react-in-jsx-scope": "off",
+      // These two rules are important for marking JSX variables as "used":
+      "react/jsx-uses-react": "error",
+      "react/jsx-uses-vars": "error",
+      "semi": ["error", "always"],
+      "quotes": ["error", "double"],
+      "no-undef": "off"
+    }
+  }
+];
--- FrontEnd/alttabjournal/src/components/ArticleCard.jsx
+++ FrontEnd/alttabjournal/src/components/ArticleCard.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const ArticleCard = ({ title, description, slug }) => (
   <Link to={`/blog/${slug}`}>
