c0598d74221de2a8ea481ccbbbeeb0c3a74e24b4
yaswanthr7-sn <yaswanthr7@gmail.com>
2025-05-26 13:38:47 +0530

    removed CrossOrigin

--- BackEnd/alttabjournal/src/main/java/com/startnow/blog/controller/ChatController.java
+++ BackEnd/alttabjournal/src/main/java/com/startnow/blog/controller/ChatController.java
@@ -3,7 +3,6 @@ package com.startnow.blog.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,7 +13,6 @@ import com.startnow.blog.service.ChatService;
 
 @RestController
 @RequestMapping("/api/chat")
-@CrossOrigin("*")
 public class ChatController {
 
     @Autowired
